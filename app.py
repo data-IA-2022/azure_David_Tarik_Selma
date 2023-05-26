9f8e0b87925fbf9e7e068159b7ecf8109e21fd94
tarik37 <tarik.medjahed@gmail.com>
2023-05-26 16:39:19 +0200

    flask app connect to db with vault

--- app.py
+++ app.py
@@ -1,37 +1,61 @@
 import pandas as pd
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-from os.path import join, dirname
-from dotenv import load_dotenv
-from os import getenv
+
 from sqlalchemy import create_engine
 
-
-load_dotenv()  # Load environment variables from .env file
-
-# Retrieve the database URL from the environment variables
-database_url = getenv('DATABASE_URL')
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
 
 
-engine = create_engine(database_url)
+
+
+
 
 app = Flask(__name__)
+
+# Create a Key Vault SecretClient
+key_vault_url = "https://flask-app-vault2.vault.azure.net/"  # Replace with your Key Vault URL
+credential = DefaultAzureCredential()
+secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
+
+ # Retrieve the connection string from Azure Key Vault
+secret_name = "DATABASE-URL2"  # Replace with the name of your secret in Key Vault
+secret_value = secret_client.get_secret(secret_name).value
+secret_value = secret_value.replace('\\','')
+engine = create_engine(secret_value )
+print(secret_value)
+print(engine)
+
 bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
-    # Get the page parameter from the request query string
-    page = request.args.get('page', default=1, type=int)
-    offset = (page - 1) * 10  # Calculate the offset for pagination
+    # Retrieve the department number from the form submission
+    dept_number = request.args.get('dept_number', default='', type=str)
 
-    # Execute the SELECT query with LIMIT and OFFSET
-    query = "SELECT * FROM insee_dept LIMIT 10 OFFSET %s" % offset
-    df = pd.read_sql_query(query, engine)
+    if dept_number:
+        # Construct the SQL query with the department number
+        query = "SELECT * FROM insee_dept WHERE dept_num = '%s'" % dept_number
 
-    # Convert the DataFrame to an HTML table
-    table_html = df.to_html(index=False)
+        df = pd.read_sql_query(query, engine)
 
-    
-    return render_template('index.html', table_html=table_html)
+        # Convert the DataFrame to an HTML table
+        table_html = df.to_html(index=False)
+
+        return render_template('index.html', table_html=table_html, dept_number=dept_number)
+
+    else:
+        # Query the entire table with LIMIT 10
+        query = "SELECT * FROM insee_dept LIMIT 10"
+
+        df = pd.read_sql_query(query, engine)
+
+        # Convert the DataFrame to an HTML table
+        table_html = df.to_html(index=False)
+
+        return render_template('index.html', table_html=table_html, dept_number='')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
